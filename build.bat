e6e551d7114d83837d1a80a0fb3d7cfc12c8e64b
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2018-02-11 14:48:50 -0200

    Reorganize folders to include the parallel version (PM) files

--- build.bat
+++ build.bat
@@ -10,8 +10,12 @@ if errorlevel 1 (
     )
 )
 
-if not exist .\src\units (
-    mkdir .\src\units
+if not exist .\build\units_main (
+    mkdir .\build\units_main
+) 
+
+if not exist .\build\units_pm(
+    mkdir .\build\units_pm
 ) 
 
 if exist ..\electricdss\Source\Common\DSSGlobals.pas (
@@ -32,10 +36,36 @@ if exist ..\electricdss\Source\Common\DSSGlobals.pas (
         del /s lib\dss_capi.exp
         del /s lib\dss_capi.def
         del /s lib\exports.txt
+
         copy /Y ..\electricdss\Distrib\x64\klusolve.dll lib\libklusolve.dll
     ) else (
-        echo ERROR: DLL file not found. Check previous messages for possible causes.
+        echo ERROR: DSS_CAPI.DLL file not found. Check previous messages for possible causes.
+        exit /B
     )
+
+    fpc -Px86_64 @src\windows.cfg -B src\dss_capi.lpr
+    if exist lib\dsspm_capi.dll (
+        where /q dumpbin
+        if errorlevel 1 (
+            echo WARNING: dumpbin.exe is not in your path. Be sure to run this script on 
+            echo          the "x64 Native Tools Command Prompt for VS 2017" or the 
+            echo          equivalent for your Visual Studio version.
+            exit /B
+        )
+        dumpbin /exports "lib\dsspm_capi.dll" > lib\exports.txt
+        echo LIBRARY DSSPM_CAPI > lib\dsspm_capi.def
+        echo EXPORTS >> lib\dsspm_capi.def
+        for /f "skip=19 tokens=4" %%A in (lib\exports.txt) do echo %%A >> lib\dsspm_capi.def
+        lib /def:lib\dsspm_capi.def /out:lib\dsspm_capi.lib /machine:X64
+        del /s lib\dsspm_capi.exp
+        del /s lib\dsspm_capi.def
+        del /s lib\exports.txt
+        
+        copy /Y ..\electricdss\Distrib\x64\klusolve.dll lib\libklusolve.dll
+    ) else (
+        echo ERROR: DSSPM_CAPI.DLL file not found. Check previous messages for possible causes.
+    )
+    
 ) else (
     echo ERROR: Please copy or link the OpenDSS source code repository to ..\electricdss and try again.
     exit /B
--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-if [ ! -d "src/units" ]; then
-    mkdir src/units
+if [ ! -d "build/units_main" ]; then
+    mkdir build/units_main
 fi
-fpc -Px86_64 @src/linux.cfg -B src/dss_capi.lpr
+fpc -Px86_64 @src/main/linux.cfg -B src/main/dss_capi.lpr
+
+if [ ! -d "build/units_pm" ]; then
+    mkdir build/units_pm
+fi
+fpc -Px86_64 @src/pm/linux.cfg -B src/pm/dsspm_capi.lpr
