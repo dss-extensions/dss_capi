e2c7814115c43948e70b187e40d89c37405c1407
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2018-10-25 15:23:50 +0000

    Update for moving the Message handling to the global context for improving the operation of the Wait command (V8)
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@2379 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Version8/Source/Meters/EnergyMeter.pas
+++ Version8/Source/Meters/EnergyMeter.pas
@@ -2143,7 +2143,7 @@ begin
                           THEN AllocationFactor := AllocationFactor * SensorObj.PhsAllocationFactor^[ConnectedPhase];
                      End;
                ELSE
-                  WITH LoadElem Do AllocationFactor := AllocationFactor * AvgAllocFactor;
+                  WITH LoadElem Do AllocationFactor := AllocationFactor * SensorObj.AvgAllocFactor;
                End;  {CASE}
          LoadElem := BranchList.NextObject    {Next load at this bus}
          End;   {While Loadelem}
