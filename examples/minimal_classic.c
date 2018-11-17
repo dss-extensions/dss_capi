01c3c2f8efa7e5d88cc8b2b6af798acb30389b89
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2018-11-17 18:30:05 -0200

    Add minimal example code.

---
+++ examples/minimal_classic.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "dss_capi.h"
+
+int main(void)
+{
+    // For numVoltages, first `int` is the current count, 
+    // the second `int` is the allocated capacity.
+    int numVoltages[2] = {0, 0}; 
+    double *voltages = NULL;
+    int numNodes;
+    int i;
+
+    DSS_Start(0);
+    Text_Set_Command("compile master.dss");
+    Solution_Solve();
+    Circuit_Get_AllBusVolts(&voltages, numVoltages);
+
+    if (numVoltages[0] == 0)
+    {
+        return -1;
+    }
+    numNodes = numVoltages[0] / 2;
+    
+    for (i = 0; i < numNodes; ++i)
+    {
+        printf("node %d: %f + j%f\n", i, voltages[2*i], voltages[2*(i + 1)]);
+    }
+
+    // Before v0.10.0, if you needed to recall Circuit_Get_AllBusVolts,
+    // you would need to call
+    
+    // DSS_Dispose_PDouble(&voltages);
+    
+    // Since v0.10.0, even if the circuit changes, you can still use the same
+    // pointer.
+    
+    // If the current count of doubles return by AllBusVolts can fit the 
+    // previous allocated capacity, no allocations are needed!
+    
+    Solution_Solve();
+    Circuit_Get_AllBusVolts(&voltages, numVoltages);
+   
+    // As the memory for voltages is allocated in Pascal, we need to
+    // free it there, hence the call to DSS_Dispose_PDouble
+    DSS_Dispose_PDouble(&voltages);
+    
+    return 0;
+}
---
+++ examples/minimal_gr.c
@@ -0,0 +1,54 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "dss_capi.h"
+
+int main(void)
+{
+    char*** data_PPAnsiChar;
+    double** data_PDouble;
+    int32_t** data_PInteger;
+    int8_t** data_PByte;
+    int32_t* count_PPAnsiChar;
+    int32_t* count_PDouble;
+    int32_t* count_PInteger;
+    int32_t* count_PByte;
+    
+    double* voltages;
+    int numNodes;
+    int i;
+    
+    DSS_Start(0);
+    DSS_GetGRPointers(
+        &data_PPAnsiChar,
+        &data_PDouble,
+        &data_PInteger,
+        &data_PByte,
+        &count_PPAnsiChar,
+        &count_PDouble,
+        &count_PInteger,
+        &count_PByte
+    );
+    
+    Text_Set_Command("compile master.dss");
+    Solution_Solve();
+    Circuit_Get_AllBusVolts_GR();
+    
+    // The result for Circuit_Get_AllBusVolts is now in 
+    // dataPtr_PDouble[0] and countPtr_PDouble
+    
+    // Copy just the pointer for convenience, the GR mechanism
+    // in Pascal is responsible for the allocated memory.
+    voltages = data_PDouble[0];
+    
+    numNodes = count_PDouble[0]/2;
+    if (numNodes == 0)
+    {
+        return -1;
+    }
+    for (i = 0; i < numNodes; ++i)
+    {
+        printf("node %d: %f + j%f\n", i, voltages[2*i], voltages[2*(i + 1)]);
+    }
+    
+    return 0;
+}
