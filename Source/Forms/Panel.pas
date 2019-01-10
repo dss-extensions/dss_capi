e13bd7b94b6a2dc9ecbeb2612923735cd7f05f3b
rdugan <rdugan@d8739450-1e93-4ef4-a0af-c327d92816ff>
2019-01-10 23:01:00 +0000

    Files on the command line are now Compiled rather than simply executed so that the path gets changed properly.
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@2474 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/Forms/Panel.pas
+++ Source/Forms/Panel.pas
@@ -1031,8 +1031,9 @@ begin
 
 // If a command line file name give, attempt to execute the script
   If CmdLineFileFound Then Begin
-    ActiveScriptForm.Editor.SelectAll;
-    ToolButton2Click(nil);   // Execute all the commands in the window
+    DSSExecutive.Command := 'compile ' + CmdLineFileName;
+ //   ActiveScriptForm.Editor.SelectAll;
+ //   ToolButton2Click(nil);   // Execute all the commands in the window
   End;
 
 
