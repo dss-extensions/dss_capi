ebfd624af488b83ea231af8163ccc825cccd6079
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2019-01-31 23:25:16 -0200

    Debugging issues with make_metadata.sh

--- make_metadata.sh
+++ make_metadata.sh
@@ -2,6 +2,9 @@ export DSS_CAPI_VERSION=`grep DSS_CAPI_V7_VERSION include/v7/dss_capi.h | grep -
 export DSS_CAPI_REV=`git rev-parse HEAD`
 export DSS_CAPI_SVN_REV=`git --git-dir=../electricdss-src/.git log | grep -m 1 -E "trunk@[0-9]+" -o | grep -E "[0-9]+" -o`
 
+echo 'Updating src/CAPI_Metadata.pas...'
+cat src/CAPI_Metadata.pas
+
 echo "UNIT CAPI_metadata;" > src/CAPI_Metadata.pas
 echo "INTERFACE" >> src/CAPI_Metadata.pas
 echo "" >> src/CAPI_Metadata.pas
@@ -13,3 +16,6 @@ echo "" >> src/CAPI_Metadata.pas
 echo "IMPLEMENTATION" >> src/CAPI_Metadata.pas
 echo "" >> src/CAPI_Metadata.pas
 echo "END." >> src/CAPI_Metadata.pas
+
+echo 'Updated src/CAPI_Metadata.pas:'
+cat src/CAPI_Metadata.pas
