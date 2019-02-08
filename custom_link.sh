9e486b07f9244e910493f4fd2aa45245608b216b
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2019-02-08 17:20:23 -0200

    Symbol aliases: try an alternative approach on MacOS

--- custom_link.sh
+++ custom_link.sh
@@ -4,8 +4,15 @@ if [ "$#" -ne 1 ]; then
     echo "Please pass the output path as parameter to this script"
     exit 1
 fi
-sed -i 's/\(      LoadShapes_Set_Sinterval;\)/\1\n      LoadShapes_Set_SInterval;/' $1/link.res 
-sed -i 's/\(      LoadShapes_Get_sInterval;\)/\1\n      LoadShapes_Get_SInterval;/' $1/link.res 
-sed -i 's/\(^VERSION$\)/LoadShapes_Set_SInterval = LoadShapes_Set_Sinterval;\nLoadShapes_Get_SInterval = LoadShapes_Get_sInterval;\n\1/' $1/link.res 
+
+if [[ $1 == *"linux"* ]]; then
+    sed -i 's/\(      LoadShapes_Set_Sinterval;\)/\1\n      LoadShapes_Set_SInterval;/' $1/link.res 
+    sed -i 's/\(      LoadShapes_Get_sInterval;\)/\1\n      LoadShapes_Get_SInterval;/' $1/link.res 
+    sed -i 's/\(^VERSION$\)/LoadShapes_Set_SInterval = LoadShapes_Set_Sinterval;\nLoadShapes_Get_SInterval = LoadShapes_Get_sInterval;\n\1/' $1/link.res 
+else
+    echo '-alias_list' >> $1/link.res
+    echo 'src/darwin_alias_list.txt' >> $1/link.res
+    cut -d " " -f 1 'src/darwin_alias_list.txt' >> $1/linksyms.fpc
+fi
 
 $1/ppas.sh
