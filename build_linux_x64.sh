c436f9c0b88a45603c5b915b098016b9676725b0
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2019-02-08 15:15:15 -0200

    Modify the default linker script to include custom aliases.

--- build_linux_x64.sh
+++ build_linux_x64.sh
@@ -4,11 +4,13 @@ if [ ! -d "build/units_v7_x64" ]; then
     mkdir build/units_v7_x64
 fi
 fpc -Px86_64 @src/v7/linux-x64.cfg -B src/v7/dss_capi_v7.lpr
+bash custom_link.sh lib/linux_x64/v7
 
 if [ ! -d "build/units_v8_x64" ]; then
     mkdir build/units_v8_x64
 fi
 fpc -Px86_64 @src/v8/linux-x64.cfg -B src/v8/dss_capi_v8.lpr
+bash custom_link.sh lib/linux_x64/v8
 
 if [ -n "$TRAVIS_TAG" ]; then
     mkdir -p release/dss_capi/
