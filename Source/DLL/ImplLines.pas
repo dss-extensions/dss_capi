4d116caabbf635422afbf8cfe6097f2830d60e3d
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2019-02-21 17:34:35 +0000

    Documentation updated in alphabetical order and fixing 0.0 result when using COM/DLL for getting the season rating
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@2523 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/DLL/ImplLines.pas
+++ Source/DLL/ImplLines.pas
@@ -932,7 +932,7 @@ begin
           Result  :=  TLineObj(ActiveCircuit[ActiveActor].ActiveCktElement).ratings^[RatingIdx];
       End
       else
-        Result  := 0.0;
+        Result  := TLineObj(ActiveCircuit[ActiveActor].ActiveCktElement).NormAmps;
     End;
   End
   else
