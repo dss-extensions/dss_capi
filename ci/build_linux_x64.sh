eb66effe3364bac4af8f5ac24dbd34983e403a7c
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2019-03-06 00:08:44 -0300

    Remove Docker for Linux x64, closes #39
    Docker is still used for Linux x86.

--- ci/build_linux_x64.sh
+++ ci/build_linux_x64.sh
@@ -1,6 +1,24 @@
 set -e -x
 
+# Install Free Pascal
+cd ..
+wget https://sourceforge.net/projects/freepascal/files/Linux/3.0.4/fpc-3.0.4.x86_64-linux.tar/download -Ofpc.tar -q
+tar xf fpc.tar
+cd fpc-3.0.4.x86_64-linux
+echo > fpc_install_options.txt
+echo n >> fpc_install_options.txt
+echo n >> fpc_install_options.txt
+echo n >> fpc_install_options.txt
+sh ./install.sh < fpc_install_options.txt
+export PATH=$PATH:~/fpc-3.0.4/bin
+
+cd ../dss_capi
+
+# Download DSS-Extensions KLUSolve
+wget "${KLUSOLVE_URL}" -Oklusolve.tar.gz -q
+tar zxf klusolve.tar.gz
+cp -r klusolve/lib/* ./lib/
+
 # Build DSS C-API
-cd /io/dss_capi
 bash make_metadata.sh
 bash build_linux_x64.sh
