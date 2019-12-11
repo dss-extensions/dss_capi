513a9aff30e3a08d5c29e00e415d31ae3ecde52d
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2019-12-11 19:59:31 +0000

    describe the starting and freeing of UUID list
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@2787 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Version7/Source/Executive/ExecCommands.pas
+++ Version7/Source/Executive/ExecCommands.pas
@@ -408,7 +408,7 @@ Begin
                         'ClearBusMarkers    !...Clears any previous bus markers'+CRLF+
                         'AddBusMarker Bus=Mybusname code=5 color=Red size=3'+CRLF+CRLF+
                         'You can use any of the standard color names  or RGB numbers. See Help on C1 property in Plot command.';
-     CommandHelp[86] := 'Read UUIDs (v4) for class names. Tab or comma-delimited file with full object name and UUID';
+     CommandHelp[86] := 'Read UUIDs (v4) for class names and other CIM objects. Tab or comma-delimited file with full object name (or key) and UUID. Side effect is to start a new UUID list for the Export CIM100 command; the UUID list is freed after the Export UUIDs command.';
      CommandHelp[87] := 'Set load and generator object kv to agree with the bus they are connected to using the bus voltage base and connection type.';
      CommandHelp[88] := 'Convert all Loadshapes presently loaded into either files of single or files of double. '+
                         'Usually files of singles are adequate precision for loadshapes.  Syntax:'+CRLF+CRLF+
