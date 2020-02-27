795b68d7d9c6bd475d51c4709bfed9336b3609c2
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2020-02-27 18:20:56 -0300

    Disable builds of v8 variation -- will be completely merged into v7 in the coming months.

--- build_linux_x86.sh
+++ build_linux_x86.sh
@@ -6,11 +6,11 @@ fi
 fpc -Pi386 @src/v7/linux-x86.cfg -B src/v7/dss_capi_v7.lpr
 bash custom_link.sh lib/linux_x86
 
-if [ ! -d "build/units_v8_x86" ]; then
-    mkdir build/units_v8_x86
-fi
-fpc -Pi386 @src/v8/linux-x86.cfg -B src/v8/dss_capi_v8.lpr
-bash custom_link.sh lib/linux_x86
+# if [ ! -d "build/units_v8_x86" ]; then
+    # mkdir build/units_v8_x86
+# fi
+# fpc -Pi386 @src/v8/linux-x86.cfg -B src/v8/dss_capi_v8.lpr
+# bash custom_link.sh lib/linux_x86
 
 mkdir -p release/dss_capi/lib
 cp -R lib/linux_x86 release/dss_capi/lib/linux_x86
--- build_macos_x64.sh
+++ build_macos_x64.sh
@@ -15,17 +15,17 @@ NEW_LIBKLUSOLVE="@loader_path/./libklusolve.dylib"
 install_name_tool -change "$CURRENT_LIBKLUSOLVE" "$NEW_LIBKLUSOLVE" "$DSS_CAPI_LIB"
 install_name_tool -id "@loader_path/./libdss_capi_v7.dylib" "$DSS_CAPI_LIB"
 
-if [ ! -d "build/units_v8_x64" ]; then
-    mkdir build/units_v8_x64
-fi
-fpc -Px86_64 @src/v8/darwin-x64.cfg -B src/v8/dss_capi_v8.lpr
-bash custom_link.sh lib/darwin_x64
+# if [ ! -d "build/units_v8_x64" ]; then
+    # mkdir build/units_v8_x64
+# fi
+# fpc -Px86_64 @src/v8/darwin-x64.cfg -B src/v8/dss_capi_v8.lpr
+# bash custom_link.sh lib/darwin_x64
 
-DSS_CAPI_LIB="lib/darwin_x64/libdss_capi_v8.dylib"
-CURRENT_LIBKLUSOLVE=`otool -L "$DSS_CAPI_LIB" | grep libklusolve | cut -f 1 -d ' ' | sed $'s/^[ \t]*//'`
-NEW_LIBKLUSOLVE="@loader_path/./libklusolve.dylib"
-install_name_tool -change "$CURRENT_LIBKLUSOLVE" "$NEW_LIBKLUSOLVE" "$DSS_CAPI_LIB"
-install_name_tool -id "@loader_path/./libdss_capi_v8.dylib" "$DSS_CAPI_LIB"
+# DSS_CAPI_LIB="lib/darwin_x64/libdss_capi_v8.dylib"
+# CURRENT_LIBKLUSOLVE=`otool -L "$DSS_CAPI_LIB" | grep libklusolve | cut -f 1 -d ' ' | sed $'s/^[ \t]*//'`
+# NEW_LIBKLUSOLVE="@loader_path/./libklusolve.dylib"
+# install_name_tool -change "$CURRENT_LIBKLUSOLVE" "$NEW_LIBKLUSOLVE" "$DSS_CAPI_LIB"
+# install_name_tool -id "@loader_path/./libdss_capi_v8.dylib" "$DSS_CAPI_LIB"
 
 mkdir -p release/dss_capi/lib
 cp -R lib/darwin_x64 release/dss_capi/lib/darwin_x64
