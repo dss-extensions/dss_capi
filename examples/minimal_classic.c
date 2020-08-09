1f10ca6cdadcf2d5921b5617b55514bf597803be
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2020-08-09 10:00:26 -0300

    Examples: Update to the current API and add example of error handling.
    Closes dss-extensions/dss_capi#67

--- examples/minimal_classic.c
+++ examples/minimal_classic.c
@@ -1,18 +1,32 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "dss_capi.h"
 
 int main(void)
 {
     // For numVoltages, first `int` is the current count, 
     // the second `int` is the allocated capacity.
-    int numVoltages[2] = {0, 0}; 
+    int64_t numVoltages[2] = {0, 0}; 
     double *voltages = NULL;
     int numNodes;
     int i;
+    int32_t* errorPtr;
 
     DSS_Start(0);
+    errorPtr = Error_Get_NumberPtr();
+    
     Text_Set_Command("compile master.dss");
+    // Ideally you should check for errors for every significant API call.
+    // For conciseness, we'll only check it here. Run in a folder without
+    // a "master.dss" to see it working
+    if (*errorPtr)
+    {
+        printf(Error_Get_Description());
+        printf("\n");
+        return EXIT_FAILURE;
+    }
+    
     Solution_Solve();
     Circuit_Get_AllBusVolts(&voltages, numVoltages);
 
@@ -45,5 +59,5 @@ int main(void)
     // free it there, hence the call to DSS_Dispose_PDouble
     DSS_Dispose_PDouble(&voltages);
     
-    return 0;
+    return EXIT_SUCCESS;
 }
--- examples/minimal_gr.c
+++ examples/minimal_gr.c
@@ -1,35 +1,49 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "dss_capi.h"
 
 int main(void)
 {
-    char*** data_PPAnsiChar;
+    float** data_PSingle;
     double** data_PDouble;
     int32_t** data_PInteger;
     int8_t** data_PByte;
-    int32_t* count_PPAnsiChar;
-    int32_t* count_PDouble;
-    int32_t* count_PInteger;
-    int32_t* count_PByte;
+    int64_t* count_PSingle;
+    int64_t* count_PDouble;
+    int64_t* count_PInteger;
+    int64_t* count_PByte;
     
     double* voltages;
     int numNodes;
     int i;
+    int32_t *errorPtr;
     
     DSS_Start(0);
     DSS_GetGRPointers(
-        &data_PPAnsiChar,
+        &data_PSingle,
         &data_PDouble,
         &data_PInteger,
         &data_PByte,
-        &count_PPAnsiChar,
+        &count_PSingle,
         &count_PDouble,
         &count_PInteger,
         &count_PByte
     );
     
+    errorPtr = Error_Get_NumberPtr();
+
     Text_Set_Command("compile master.dss");
+    // Ideally you should check for errors for every significant API call.
+    // For conciseness, we'll only check it here. Run in a folder without
+    // a "master.dss" to see it working
+    if (*errorPtr)
+    {
+        printf(Error_Get_Description());
+        printf("\n");
+        return EXIT_FAILURE;
+    }
+    
     Solution_Solve();
     Circuit_Get_AllBusVolts_GR();
     
