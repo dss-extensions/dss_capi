db6fd92936f636370b83cda1b4e3e58bc5cde93d
Ed Beroset <beroset@ieee.org>
2021-08-07 16:44:04 -0400

    Fix #97 by allowing either plain link.res or link{pid}.res file.

--- custom_link.sh
+++ custom_link.sh
@@ -5,16 +5,18 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+resfile=$(ls "$1"/*.res)
+
 if [[ $1 == *"linux"* ]]; then
-    sed -i 's/\(      LoadShapes_Set_Sinterval;\)/\1\n      LoadShapes_Set_SInterval;/' $1/link.res 
-    sed -i 's/\(      LoadShapes_Get_sInterval;\)/\1\n      LoadShapes_Get_SInterval;/' $1/link.res 
-    sed -i 's/\(^VERSION$\)/LoadShapes_Set_SInterval = LoadShapes_Set_Sinterval;\nLoadShapes_Get_SInterval = LoadShapes_Get_sInterval;\n\1/' $1/link.res 
+    sed -i 's/\(      LoadShapes_Set_Sinterval;\)/\1\n      LoadShapes_Set_SInterval;/' "$resfile"
+    sed -i 's/\(      LoadShapes_Get_sInterval;\)/\1\n      LoadShapes_Get_SInterval;/' "$resfile"
+    sed -i 's/\(^VERSION$\)/LoadShapes_Set_SInterval = LoadShapes_Set_Sinterval;\nLoadShapes_Get_SInterval = LoadShapes_Get_sInterval;\n\1/' "$resfile"
 else
-    echo '-alias_list' >> $1/link.res
-    echo 'src/darwin_alias_list.txt' >> $1/link.res
+    echo '-alias_list' >> "$resfile"
+    echo 'src/darwin_alias_list.txt' >> "$resfile"
     cut -d " " -f 2 'src/darwin_alias_list.txt' >> $1/linksyms.fpc
 fi
 
 $1/ppas.sh
-rm -f $1/ppas.sh $1/link.res $1/linksyms.fpc
+rm -f $1/ppas.sh "$resfile" $1/linksyms.fpc
 
