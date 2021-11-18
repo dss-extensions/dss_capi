fd91d9dd9ac733b4463e637e1fadc08fb5fea417
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2021-11-18 20:15:52 +0000

    Updating licensing on files
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3315 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/Common/DSSClass.pas
+++ Source/Common/DSSClass.pas
@@ -2,7 +2,7 @@
 unit DSSClass;
 {
     ----------------------------------------------------------
-  Copyright (c) 2008-2015, Electric Power Research Institute, Inc.
+  Copyright (c) 2008-2021, Electric Power Research Institute, Inc.
   All rights reserved.
   ----------------------------------------------------------
 }
