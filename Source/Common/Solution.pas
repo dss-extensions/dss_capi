a77d96ec3308707b97e8585ce248811a0f97f255
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2021-12-04 00:04:03 +0000

    cleaned up the last 4 warnings
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3345 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/Common/Solution.pas
+++ Source/Common/Solution.pas
@@ -2815,7 +2815,9 @@ begin
   {$IFDEF MSWINDOWS}              // Only for windows
 //  Parallel.Set_Process_Priority(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
 //  Parallel.Set_Thread_affinity(handle,local_CPU);     // Commenting this line to give to the actor affinity to any CPU by default
+  Parallel := TParallel_Lib.Create;
   Parallel.Set_Thread_Priority(handle,THREAD_PRIORITY_TIME_CRITICAL);
+  FreeAndNil (Parallel);
   {$ENDIF}
 end;
 {$ELSE}
@@ -3011,8 +3013,10 @@ var
 Begin
   ActorCPU[ActorID] :=  CPU;
   {$IFDEF MSWINDOWS}
+  Parallel := TParallel_Lib.Create;
   Parallel.Set_Thread_affinity(handle,CPU_Cores, CPU);
   Parallel.Set_Thread_Priority(handle,THREAD_PRIORITY_TIME_CRITICAL);
+  FreeAndNil (Parallel);
   {$ENDIF}
 End;
 {$ELSE}
