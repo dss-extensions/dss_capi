4dd7c34fa1a9a033d7cedd5596f4c34fb97ccb8b
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2022-03-30 16:08:11 +0000

    adding N to ungrounded TransformerTankEnd.orderedPhases
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3398 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/Common/ExportCIMXML.pas
+++ Source/Common/ExportCIMXML.pas
@@ -1046,22 +1046,18 @@ procedure XfmrTankPhasesAndGround (fprf: ProfileChoice; eprf: ProfileChoice; pXf
 var
   ordered_phs, phs: String;
   j1, j2: Integer;
-  reverse_ground, wye_ground: Boolean;
-//  j, jmax: Integer;
+  reverse_ground, wye_ground, delta, wye_unground: Boolean;
 begin
-//  writeln(Format ('Xfmr Tank: %s end: %d Nconds: %d Nterms: %d Nphases: %d', [pXf.LocalName, bus, pXf.Nconds, pXf.Nterms, pXf.Nphases]));
-  // interpret the grounding and reversal connections
-//  jmax := pXf.NConds * pXf.NTerms;
-//  for j := 1 to jmax do begin
-//    writeln(Format ('  j: %d, noderef^[j]: %d', [j, pXf.NodeRef^[j]]));
-//  end;
   j1 := (bus-1) * pXf.NConds + 1;
   j2 := j1 + pXf.Nphases;
   reverse_ground := False;
   wye_ground := False;
+  wye_unground := False;
+  delta := False;
 //  writeln(Format('  Testing %d and %d', [j1, j2]));
   if (pXf.Winding^[bus].Connection = 1) then begin // delta
     BooleanNode (fprf, 'TransformerEnd.grounded', false);
+    delta := True;
   end else if (pXf.NodeRef^[j2] = 0) then begin // last conductor is grounded solidly
     BooleanNode (FunPrf, 'TransformerEnd.grounded', true);
     DoubleNode (EpPrf, 'TransformerEnd.rground', 0.0);
@@ -1074,6 +1070,7 @@ begin
     reverse_ground := True;
   end else if (pXf.Winding^[bus].Rneut < 0.0) then begin // probably wye ungrounded
     BooleanNode (FunPrf, 'TransformerEnd.grounded', false);
+    wye_unground := True;
   end else begin // not delta, not wye solidly grounded or ungrounded
     BooleanNode (FunPrf, 'TransformerEnd.grounded', true);
     DoubleNode (EpPrf, 'TransformerEnd.rground', pXf.Winding^[bus].Rneut);
@@ -1087,7 +1084,10 @@ begin
   else if reverse_ground then
     ordered_phs := 'N' + ordered_phs
   else if wye_ground then
+    ordered_phs := ordered_phs + 'N'
+  else if wye_unground then
     ordered_phs := ordered_phs + 'N';
+
   FD.WriteCimLn (fprf, Format ('  <cim:TransformerTankEnd.orderedPhases rdf:resource="%s#OrderedPhaseCodeKind.%s"/>',
     [CIM_NS, ordered_phs]));
 end;
