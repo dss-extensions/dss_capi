9cf70e2c381e39b3f906d5825b991a802d836de8
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2022-05-12 20:49:10 +0000

    change from rdf:ID to rdf:about in CIM export; remove leading underscores from mRID
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3449 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/General/NamedObject.pas
+++ Source/General/NamedObject.pas
@@ -1,7 +1,7 @@
 unit NamedObject;
 {
   ----------------------------------------------------------
-  Copyright (c) 2009-2021, Electric Power Research Institute, Inc.
+  Copyright (c) 2009-2022, Electric Power Research Institute, Inc.
   All rights reserved.
   ----------------------------------------------------------
 }
@@ -70,7 +70,7 @@ var
   s: String;
 begin
   s := GUIDToString (UUID);
-  Result := '_' + MidStr (s, 2, Length (s)-2);
+  Result := MidStr (s, 2, Length (s)-2);
 end;
 
 constructor TNamedObject.Create(ClassName:String);
