e445908611d2f37648b682743a0ac3324040268c
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2023-10-21 02:56:29 +0000

    stack tracing for exceptions
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3678 d8739450-1e93-4ef4-a0af-c327d92816ff

---
+++ Version8/Source/Shared/ExceptionTrace.pas
@@ -0,0 +1,52 @@
+unit ExceptionTrace;
+
+interface
+
+Uses sysutils;
+
+procedure ShowHeapUsage;
+procedure DumpExceptionCallStack(E: Exception);
+
+implementation
+
+procedure DumpExceptionCallStack(E: Exception);
+var
+  I: Integer;
+  Frames: PPointer;
+  Report: string;
+begin
+{$IFDEF FPC}
+  Report := 'Program exception! ' + LineEnding +
+    'Stacktrace:' + LineEnding + LineEnding;
+  if E <> nil then begin
+    Report := Report + 'Exception class: ' + E.ClassName + LineEnding +
+    'Message: ' + E.Message + LineEnding;
+  end;
+  Report := Report + BackTraceStrFunc(ExceptAddr);
+  Frames := ExceptFrames;
+  for I := 0 to ExceptFrameCount - 1 do
+    Report := Report + LineEnding + BackTraceStrFunc(Frames[I]);
+  Writeln(Report);
+  Halt; // End of program execution
+{$ELSE}
+// TODO: https://www.testrail.com/blog/working-with-delphis-new-exception-stacktrace/
+  Report := 'Exception Class:' + E.ClassName + ' Message:' + E.Message;
+  Writeln (Report);
+{$ENDIF}
+end;
+
+procedure ShowHeapUsage;
+{$IFDEF FPC}
+var
+   hstat: TFPCHeapStatus;
+   s: string;
+{$ENDIF}
+begin
+{$IFDEF FPC}
+  hstat := GetFPCHeapStatus;
+  s := Format('Heap Memory Used: %dK',[hstat.CurrHeapUsed div 1024]);
+  Writeln(s);
+{$ENDIF}
+end;
+
+end.
