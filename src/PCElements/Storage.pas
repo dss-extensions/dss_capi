e97cbc8c4ba6add271b7809c5d0b4d118cdd562e
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2024-11-29 21:28:11 -0300

    Storage: port SVN r3582, "Fixing initialization issue when setting safe voltage = 0 for IBR dynamics." by davismont.
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3582 d8739450-1e93-4ef4-a0af-c327d92816ff

--- src/PCElements/Storage.pas
+++ src/PCElements/Storage.pas
@@ -2801,7 +2801,7 @@ begin
     with StorageVars, dynVars do
     begin
         ComputePresentkW();
-        IMaxPhase := (kW_out / BasekV) / NumPhases;
+        IMaxPhase := (FkVArating / BasekV) / NumPhases;
         for i := 0 to (NumPhases - 1) do
         begin
             if FState = STORE_DISCHARGING then
