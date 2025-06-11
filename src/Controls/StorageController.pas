717a7ad660a0f8da7d74b148a8f6d384a07fe992
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2025-06-10 22:00:18 -0300

    StorageController: port SVN r4058 "Fixing out of synchronism in in StorageController between properties %kWBandLow and kWBandLow when kWBandLow is specified." by celsorocha

--- src/Controls/StorageController.pas
+++ src/Controls/StorageController.pas
@@ -544,7 +544,7 @@ begin
                 Casemult := 1.0;
 
             HalfkWBandLow := FkWBandLow / 2.0 * Casemult;
-            FpctkWBand := FkWBandLow / FkWTarget * 100.0; // sync FpctkWBandLow
+            FpctkWBandLow := FkWBandLow / FkWTargetLow * 100.0; // sync FpctkWBandLow
         end;
         ord(TProp.ModeDischarge):
             if DischargeMode = MODEFOLLOW then
